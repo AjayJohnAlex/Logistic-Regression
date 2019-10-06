85a1e8b997117f72541f5f7121d26f6dc2a6f710
AjayJohnAlex <ajay.alex96@gmail.com>
2019-10-06 23:41:08 +0530

    Classification on Sklearn's cancer dataset using RandomForestClassification and MLPClassifier

--- classification.py
+++ classification.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 from sklearn.ensemble import RandomForestClassifier
 
 rfc = RandomForestClassifier()
-
+# training the labels with RandomForestClassifier object
 rfc.fit(X_train,y_train)
 
 print('Train Score ',rfc.score(X_train,y_train))
