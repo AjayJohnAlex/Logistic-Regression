076188b6d76db36cfb13126c0433800dac3c6bd6
AjayJohnAlex <ajay.alex96@gmail.com>
2019-10-06 21:20:06 +0530

    Classification on Sklearn's cancer dataset using RandomForestClassification and MLPClassifier

---
+++ classification.py
@@ -0,0 +1,62 @@
+# importing libraries
+import numpy as np
+
+
+# import data 
+from sklearn.datasets import load_breast_cancer
+# load dataset
+data  = load_breast_cancer()
+
+# print these commands to get an idea of the dataset
+# print(type(data))
+# print(data.DESCR)
+
+from sklearn.model_selection import train_test_split
+
+X = data.data
+y = data.target
+
+# Spilting the data into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42) 
+
+from sklearn.ensemble import RandomForestClassifier
+
+rfc = RandomForestClassifier()
+
+rfc.fit(X_train,y_train)
+
+print('Train Score ',rfc.score(X_train,y_train))
+
+print('Test score ' ,rfc.score(X_test,y_test))
+
+predictions = rfc.predict(X_test)
+# Manual prediction of accuracy 
+N = len(y_test)
+print("Manual Prediction ",np.sum(predictions == y_test)/N)
+
+from sklearn.metrics import accuracy_score
+
+print("Accuracy Score" ,accuracy_score(y_test,predictions))
+
+# using a Deep Learning model
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+
+# Need to give the NN classifier a parameter #max_iter=500 (It didnt converge as NN is need more time)
+# ERROR : ConvergenceWarning: Stochastic Optimizer: Maximum iterations (200) reached and the
+# optimization hasn't converged yet.
+
+mlp = MLPClassifier(max_iter=500)
+standardScale = StandardScaler()
+
+X_train2 = standardScale.fit_transform(X_train)
+X_test2 = standardScale.transform(X_test)
+
+mlp.fit(X_train2,y_train)
+
+print("NN train scsore",mlp.score(X_train2,y_train))
+print("NN test scsore",mlp.score(X_test2,y_test))
+
+mlp_pred = mlp.predict(X_test2)
+
+print("NN prediction score",accuracy_score(y_test,mlp_pred))
